65bd648488e521d694adf7d500a4d84ebb9763bf
zakariabinsaifullah <zbinsaifullah@gmail.com>
2021-06-16 22:46:57 +0600

    Button Block added

---
+++ src/block2/attributes.js
@@ -0,0 +1,23 @@
+const attributes= {
+    label: {
+        type: 'string',
+    },
+    link: {
+        type: 'string',
+        default: '#'
+    },
+    type: {
+        type: 'string',
+        default: 'solid'
+    },
+    style: {
+        type: 'string',
+        default: 'primary'
+    },
+    newTab: {
+        type: 'boolean',
+        default: false
+    }
+};
+
+export default attributes; 
